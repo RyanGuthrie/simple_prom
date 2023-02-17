33aa02fc7b565181ae47c9542fe7ab358a57413e
Ryan Guthrie <ryan@arlojeep.com>
2023-02-17 09:51:51 -0700

    Update Readme, move HTTP handler for exposing metrics into main struct

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# simple-prom
+# simple_prom
 Simple wrapper around the GoLang client for Prometheus
+
+## Usage
+go.mod
+
+    require github.com/RyanGuthrie/simple_prom
+
+### Exposing `/metrics` endpoint
+
+Add the `simple_prom.Metrics.Handler` handler to the HTTP server, eg:
+
+    mux.Handle("/metrics", simple_prom.Metrics.Handler)
+
+### Creating new metrics
+
+Creating new metrics, for example a counter:
+
+	requestCounter := simple_prom.Metrics.NewCounter(prometheus.CounterOpts{
+		Namespace: "http",
+		Subsystem: "overall",
+		Name:      "requests",
+		Help:      "Number of HTTP requests made overall across all endpoints",
+	})
--- simpleprom.go
+++ simpleprom.go
@@ -4,25 +4,38 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
 )
 
 type metricsServer struct {
-	Registry *prometheus.Registry
+	// HTTP Handler for exposing Prometheus Metrics (recommended to bind to the /metrics endpoint)
+	Handler *http.Handler
+
+	registry *prometheus.Registry
+	factory  *promauto.Factory
 }
 
-var Metrics = metricsServer{Registry: prometheus.NewRegistry()}
+// Metrics - container used for creating counters, histograms, gauges, etc...
+var Metrics = metricsServer{registry: prometheus.NewRegistry()}
 
 func init() {
-	Metrics.Registry.MustRegister(
+	Metrics.registry.MustRegister(
 		collectors.NewGoCollector(),
 		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
 	)
+
+	factory := promauto.With(Metrics.registry)
+	Metrics.factory = &factory
+
+	metricsHandler := promhttp.HandlerFor(Metrics.registry, promhttp.HandlerOpts{Registry: Metrics.registry})
+	Metrics.Handler = &metricsHandler
 }
 
 func (metrics metricsServer) NewCounter(opts prometheus.CounterOpts) prometheus.Counter {
-	return promauto.With(metrics.Registry).NewCounter(opts)
+	return metrics.factory.NewCounter(opts)
 }
 
 func (metrics metricsServer) NewHistogram(opts prometheus.HistogramOpts) prometheus.Histogram {
-	return promauto.With(metrics.Registry).NewHistogram(opts)
+	return metrics.factory.NewHistogram(opts)
 }
